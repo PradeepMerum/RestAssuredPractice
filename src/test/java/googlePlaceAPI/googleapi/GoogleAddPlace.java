5da987a433a5cf7388078ff59c85ad54e332120d
Muralim93 <41182996+Muralim93@users.noreply.github.com>
2024-04-24 08:54:11 -0500

    added update and delete http requests

--- src/test/java/googlePlaceAPI/googleapi/GoogleAddPlace.java
+++ src/test/java/googlePlaceAPI/googleapi/GoogleAddPlace.java
@@ -23,9 +23,28 @@ public class GoogleAddPlace {
 		
 		String getHttpResponse = given().queryParam("key", "qaclick123").queryParam("place_id", placeId)
 		.when().get("maps/api/place/get/json")
-		.then().log().all().assertThat().assertThat().statusCode(200).extract().toString();
+		.then().log().all().assertThat().assertThat().statusCode(200).extract().asString();
 		
 		JsonPath js1 = new JsonPath(getHttpResponse);
+		
+		String oldaddress = js1.getString("address");
+		System.out.println(oldaddress);
+		
+		//Updating the address using PUT http request
+		
+		String putHttpResponse = given().log().all().queryParam("key", "qaclick123").body("{\r\n"
+				+ "\"place_id\":\""+placeId+"\",\r\n"
+				+ "\"address\":\"70 Summer walk, USA\",\r\n"
+				+ "\"key\":\"qaclick123\"\r\n"
+				+ "}")
+		.when().put("maps/api/place/update/json")
+		.then().log().all().assertThat().statusCode(200).extract().asString();
+		
+		given().log().all().queryParam("key", "qaclick123").body("{\r\n"
+				+ "    \"place_id\":\""+placeId+"\"\r\n"
+				+ "}")
+		.when().delete("maps/api/place/delete/json")
+		.then().log().all().assertThat().statusCode(200).extract().asString();
 		
 	}
 }
